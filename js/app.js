092472bd1be8166fc6f4a650c4ab635143484b96
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-06-10 13:39:36 -1000

    Started getting into CSS

--- js/app.js
+++ js/app.js
@@ -15,40 +15,41 @@ console.log("yeah buddy ")
 
   function loadPics(){
     for (var i = 0 ; i <26; i++)  {
+      var contain=document.createElement('div')
+      contain.className="contain";
+      document.getElementsByClassName('flexcontainer')[0].appendChild(contain)
+
       var response=  JSON.parse(this.responseText);
       var pic=document.createElement('IMG')
        pic.setAttribute("src", response.data.children[i].data.thumbnail);
-      pic.classname=pic;
-
+      pic.className="pic";
       console.log(response)
-      pontainer.appendChild(pic)
+      contain.appendChild(pic)
 
-      var tontainer=document.createElement('div')
-      tontainer.classsname=tontainer;
-      pontainer.appendChild(tontainer)
+
 
     var tRes=JSON.parse(this.responseText);
     var title=document.createElement('h2')
-    title.classname=title
+    title.className="title"
     title.innerHTML=tRes.data.children[i].data.title
-    tontainer.appendChild(title)
+    contain.appendChild(title)
 
       var aRes=JSON.parse(this.responseText);
       var author=document.createElement('h5')
-      author.classname=author
+      author.className="author"
       author.innerHTML="By " +aRes.data.children[i].data.author
       title.appendChild(author)
 
       var timeRes= JSON.parse(this.responseText);
       var time=document.createElement('h6')
-      time.classname=time
+      time.className="time"
       var timeTimes= new Date(aRes.data.children[i].data.created*1000)
       time.innerHTML=timeTimes
       author.appendChild(time)
 
       var comRes=JSON.parse(this.responseText);
       var comment=document.createElement('h5')
-      comment.classname=comment
+      comment.className="comment"
       comment.innerHTML="Comments: "+comRes.data.children[i].data.num_comments
       time.appendChild(comment)
 }
