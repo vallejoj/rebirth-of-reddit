d006a034047ef7ac2507bc1a33fb752c08c75635
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-06-13 13:35:03 -1000

    Changed some CSS started working on buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,14 @@
 (function(){
-console.log("yeah buddy ")
-
 
   var oReq = new XMLHttpRequest();
   oReq.addEventListener('load',loadPics )
   oReq.open('GET', ' https://www.reddit.com/r/ArchitecturePorn/.json')
   oReq.send()
 
-// var pReq= new XMLHttpRequest();
-// pReq.addEventListener('load',loadPics )
-// pReq.open('GET', ' https://www.reddit.com/r/interestingasfuck.json')
-// pReq.send()
-
+document.getElementById('board').addEventListener('click', randomButton)
 
   function loadPics(){
-    for (var i = 0 ; i <10; i++)  {
+    for (var i = 0 ; i <1; i++)  {
       var contain=document.createElement('div')
       contain.className="contain";
       document.getElementsByClassName('flexbox-container')[0].appendChild(contain)
@@ -83,6 +77,71 @@ console.log("yeah buddy ")
       comment.className="comment"
       comment.innerHTML="Comments: "+comRes.data.children[i].data.num_comments
       time.appendChild(comment)
+}
+}
+
+
+function randomButton(){
+var pReq= new XMLHttpRequest();
+pReq.addEventListener('load',randomReddit )
+pReq.open('GET', ' https://www.reddit.com/r/EarthPorn/.json')
+pReq.send()
+}
+
+
+function randomReddit(){
+  oReq.addEventListener('unload', randomReddit)
+  for (var i = 0 ; i <10; i++)  {
+    var contain=document.createElement('div')
+    contain.className="contain";
+    document.getElementsByClassName('flexbox-container')[0].appendChild(contain)
+
+    var response=  JSON.parse(this.responseText);
+    var pic=document.createElement('IMG')
+     pic.setAttribute("src", response.data.children[i].data.url);
+    pic.className="pic";
+    contain.appendChild(pic)
+
+    var wordContainer=document.createElement('div')
+    wordContainer.className="container";
+    contain.appendChild(wordContainer)
+
+  var tRes=JSON.parse(this.responseText);
+  var title=document.createElement('p')
+  title.className="title"
+  title.innerHTML=tRes.data.children[i].data.title
+  wordContainer.appendChild(title)
+
+
+    var aRes=JSON.parse(this.responseText);
+    var author=document.createElement('p')
+    author.className="author"
+    var auth= aRes.data.children[i].data.author
+    var timeTimes= aRes.data.children[i].data.created_utc
+    console.log(timeTimes);
+
+    author.innerHTML="By: " + auth +
+    " • "+ "7 months ago";
+
+    contain.appendChild(author)
+
+    var timeRes= JSON.parse(this.responseText);
+    var time=document.createElement('p')
+    time.className="time"
+
+
+
+    author.appendChild(time)
+
+    var comRes=JSON.parse(this.responseText);
+    var comment=document.createElement('p')
+    comment.className="comment"
+    comment.innerHTML="Comments: "+comRes.data.children[i].data.num_comments
+    time.appendChild(comment)
+
+
+
+
 }
   }
 
